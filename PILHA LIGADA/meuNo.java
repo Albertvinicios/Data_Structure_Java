630e018d6c606714cb278300dad2ad2aa8d9692e
Albertvinicios <albert-vinicios@hotmail.com>
2026-03-18 10:47:00 -0300

    Comentários Adicionados na descrição da Pilha

--- PILHA LIGADA/meuNo.java
+++ PILHA LIGADA/meuNo.java
@@ -1,12 +1,14 @@
-public class meuNo {
+public class meuNo {//classe para representar um nó de uma pilha ligada, onde cada nó tem um valor inteiro
+//e um ponteiro para o próximo nó
     // Atributos
     public int info;
     public meuNo proxi;
 
     //Construtores
     public meuNo(){
-        this.info = 0;
-        this.proxi = null;
+        this.info = 0;//inicializa o valor do nó com 0, onde 0 é um valor padrão para um nó vazio
+        this.proxi = null;//ponteiro para o próximo nó
+        //onde null indica que não há próximo nó, ou seja, é o último nó da pilha
     }
     public meuNo(int i){
         this.info = i;
--- PILHA LIGADA/minhaPilhaLig.java
+++ PILHA LIGADA/minhaPilhaLig.java
@@ -1,36 +1,43 @@
-public class minhaPilhaLig {
+public class minhaPilhaLig {//minha pilha ligada, onde cada nó tem um valor inteiro
+//e um ponteiro para o próximo nó
     //Atributos herdados da classe meuNo
     public meuNo topo;
     //Construtores
-    public minhaPilhaLig(){
+    public minhaPilhaLig(){ //construtor para inicializar a pilha vazia, onde o topo é null
         this.topo = null;
     }
     //getters and setters
-    //nesse caso tudo está herdado
+    //nesse caso tudo está herdado de outras classes
     //não sendo necessário inicializar getters and Setters.
-    public boolean estaVazio(){
-        if(topo == null){
-            return true;
-        }else{
-            return false;
+    public boolean estaVazio(){ //Método para verificar se a pilha está vazia
+    //onde a pilha está vazia se o topo for null.
+        if(topo == null){ //se for igual a null
+            return true; //está vazia
+        }else{ //se não
+            return false; //não está vazia
         }
     }
-    public void meuPush(int info){
-        meuNo atual = new meuNo(info);
-        if(estaVazio()){
-            topo = atual;
-        }else{
-            atual.setProxi(topo);
-            topo = atual;
-        }
+    public void meuPush(int info){//Método para adicionar elemento a pilha
+        meuNo atual = new meuNo(info); //instancia um novo nó chamado atual
+        //com a informação passada como parâmetro
+        if(estaVazio()){ //se estiver vazia, ou seja, se o topo for null
+            topo = atual; //o primeiro elemento vai ser o atual
+        }else{ //se não
+            atual.setProxi(topo); //atual aponta para o topo
+            //ou seja, o próximo elemento do atual é o topo
+            topo = atual; //após apontar, o topo passa a ser o atual, ou seja, o novo elemento da pilha
+        } 
     }
-    public int meuPop(){
-        if(estaVazio()){
-            return -1;
-        }else{
-            int temp = topo.getInfo();
-            topo = topo.getProxi();
-            return temp;
+    public int meuPop(){ //Método para remover elemento da pilha, onde o elemento removido é o topo
+        if(estaVazio()){ //se estiver vazia, não tem o que remover
+            return -1; //retorna menos -1 para indicar que a pilha está vazia
+        }else{ //se não estiver vazia, ou seja, se o topo for diferente de null
+            int temp = topo.getInfo(); //armazena o valor do topo em uma variável temporária
+            topo = topo.getProxi();//aponta o topo para o próximo elemento, ou seja null
+            //ou seja, o próximo elemento passa a ser o topo null, o valor anterior perde a referencia
+            //ou seja, é removido da pilha.
+            return temp; //retorna o valor do topo que foi removido, ou seja
+            //o valor armazenado na variável temporária
         }
     }
     @Override //Método de impressão
